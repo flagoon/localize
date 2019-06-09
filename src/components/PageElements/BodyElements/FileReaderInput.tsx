6b2fef9d8fd5ff761dbc2640b288fd318d9e7c9e
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-06-09 21:56:45 +0200

    Added filereader component with parser.

---
+++ src/components/PageElements/BodyElements/FileReaderInput.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEvent } from 'react';
+import { FileInput, FileLabel } from './FileReaderInput.styled';
+
+import { xmlDataExtractor } from './xmlDataExtractor';
+
+export default function FileReaderInput(): JSX.Element {
+  const handleFile = (file: string): void => {
+    xmlDataExtractor(file);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const fileReader: FileReader = new FileReader();
+    let file: Blob = new Blob();
+    if (e.target.files) {
+      file = e.target.files[0];
+    }
+    fileReader.onload = () => {
+      handleFile(fileReader.result as string);
+    };
+    fileReader.readAsText(file);
+  };
+
+  return (
+    <>
+      <FileInput
+        type="file"
+        id="fileHandler"
+        name="fileHandler"
+        accept=".xml"
+        onChange={handleChange}
+      />
+      <FileLabel htmlFor="fileHandler">Dodaj plik raportu</FileLabel>
+    </>
+  );
+}
