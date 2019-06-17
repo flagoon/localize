8cd1666e9e388fd0986d056c95af3ca4e75ea000
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-06-17 22:51:48 +0200

    Removed faulty functions

--- src/components/PageElements/BodyElements/xmlDataExtractor.ts
+++ src/components/PageElements/BodyElements/xmlDataExtractor.ts
@@ -2,26 +2,4 @@ import parse from 'xml-parser';
 
 export function xmlDataExtractor(xmlData: string): parse.Document {
   return parse(xmlData);
-
-type PickedParseNode = Pick<parse.Node, 'name' | 'content'>;
-
-/**
- * Map()
- */
-export function recursiveDataExtractor(
-  data: parse.Node,
-): PickedParseNode | parse.Node {
-  const { children, name, content } = data;
-  if (children && children.length > 0) {
-    return { [name]: recursiveDataExtractor };
-  } else {
-    return { [name]: content };
-  }
-}
-
-export function xmlDataExtractor(xmlData: string): void {
-  const objParsedFromXML = parse(xmlData);
-  const { root } = objParsedFromXML;
-  const data = recursiveDataExtractor(root);
-  console.log(data);
 }
