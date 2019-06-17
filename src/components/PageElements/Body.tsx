68aff21c6cfd9ec3ca6f6534a9436028099e68c7
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-06-17 21:25:56 +0200

    Added images to system.

--- src/components/PageElements/Body.tsx
+++ src/components/PageElements/Body.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { PageElementWrapper } from './PageElement.styled';
 import FileReaderInput from './BodyElements/FileReaderInput';
+import LocalizeLogo from '../../assets/baner-localizepl.png';
 
 function Body(): JSX.Element {
   return (
     <PageElementWrapper>
       <FileReaderInput />
       <div>IM Body</div>
+      <img src={LocalizeLogo} alt="Localize logo" />
     </PageElementWrapper>
   );
 }
