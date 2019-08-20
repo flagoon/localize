c66e8e9c0aeb46a711e0e19116d9d541501abbf3
Pawel -Muody- Kochanek <pawel.kochanek@gmail.com>
2019-08-20 21:55:17 +0200

    Refactor for saving data.

--- src/components/PageElements/ProjectOptions/index.ts
+++ src/components/PageElements/ProjectOptions/index.ts
@@ -1,4 +1,5 @@
 import ProjectOptions from './ProjectOptions';
 import RadioButton from './RadioButton';
+import SaveButtons from './SaveButtons';
 
-export { ProjectOptions, RadioButton };
+export { ProjectOptions, RadioButton, SaveButtons };
